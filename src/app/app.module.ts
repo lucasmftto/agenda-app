319cc2cfdd8140ac1497eb00b01b435014dfed58
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-04-28 21:24:48 -0300

    criacao de formulario e incluindo meterialAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContatoComponent } from './contato/contato.component';
+import { ContatoService } from './contato.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    ContatoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
