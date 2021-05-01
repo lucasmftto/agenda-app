29615ff175ab9dac0c9f83300d297a911ff1d74f
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-05-01 19:53:38 -0300

    criacao de formulario de listar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,6 +27,11 @@ import { ContatoService } from './contato.service';
     MatToolbarModule,
     MatInputModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
     BrowserAnimationsModule
   ],
   providers: [
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ContatoService } from '../contato.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Contato } from './contato';
 
 @Component({
   selector: 'app-contato',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+  contatos: Contato[] = [];
+  colunas = ['id', 'nome', 'email', 'favorito']
+
+  constructor(
+    private service: ContatoService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+      this.montarFormulario();
+      this.listarContatos();
+  }
+
+  montarFormulario(){
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]]
+    })
+  }
+
+  listarContatos(){
+    this.service.list().subscribe(response =>{
+      this.contatos = response;
+    })
+  }
+
+  favoritar(contato: Contato){
+    this.service.favourite(contato).subscribe(response =>{
+      contato.favorito = !contato.favorito; 
+    })
+  }
+
+  submit(){
+    const formValues =  this.formulario.value;
+    const contato: Contato =  new Contato(formValues.nome, formValues.email);
+    this.service.save(contato).subscribe( resposta => {
+      let lista: Contato[] = [...this.contatos, resposta]
+      this.contatos = lista;
+    })
   }
 
 }
